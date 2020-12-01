cec9108bd7f5e73143f447b3a88566a51558d63b
Freddie Carre <freddie.carre@infinityworks.com>
2020-12-01 19:42:22 +0000

    day one

---
+++ day_1/part_1.js
@@ -0,0 +1,51 @@
+import { readFile } from '../utils/fileParser.js';
+
+const getOrderedList = () => {
+  return readFile('./day_1/input.txt')
+    .map(v => parseInt(v))
+    .filter(Number.isInteger)
+    .sort((a, b) => a - b);
+}
+
+const partOne = (total, orderedList) => {
+  if (orderedList.length < 2) {
+    return null;
+  }
+  const smallest = orderedList[0];
+  const largest = orderedList.slice(-1)[0];
+  const sum = smallest + largest;
+
+  if (sum > total) {
+    return partOne(total, orderedList.slice(0,-1));
+  }
+
+  if (sum < total) {
+    return partOne(total, orderedList.slice(1));
+  }
+
+  return [smallest, largest];
+};
+
+const partTwo = (orderedList) => {
+  try {
+    orderedList.forEach((v, i, arr) => {
+      const others = partOne(2020 - v, arr.slice(i + 1));
+
+      if (others !== null) {
+        const sum = others.reduce((a, b) => a + b) + v;
+
+        if (sum === 2020) {
+          throw { values: [v, ...others] };
+        }
+      };
+    });
+  } catch (e) {
+    return e.values; 
+  }
+};
+
+console.log("Part One: ")
+console.log(partOne(2020, getOrderedList()).reduce((a, b) => a * b));
+
+console.log("Part Two: ")
+console.log(partTwo(getOrderedList()).reduce((a, b) => a * b));
---
+++ utils/fileParser.js
@@ -0,0 +1,7 @@
+import { readFileSync } from 'fs';
+
+export const readFile = (filePath) => {
+  const file = readFileSync(filePath, 'utf8');
+  
+  return file.split('\n');
+};
