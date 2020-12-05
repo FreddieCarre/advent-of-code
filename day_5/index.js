4eb300419567e435d23e40224976479eecee9898
Freddie Carre <freddie.carre@infinityworks.com>
2020-12-05 18:26:57 +0000

    updated to use binary

--- day_5/index.js
+++ day_5/index.js
@@ -1,37 +1,17 @@
 import { readFile } from '../utils/fileParser.js';
 
-const ROWS = 127;
-const COLS = 7;
-
 const getPositions = () => {
   return readFile('./day_5/input.txt');
 };
 
 const getSeatIds = () => {
   return getPositions()
-    .map(b => {
-      return b.split('').reduce((acc, position) => {
-        if (position === 'B') {
-          acc.lRow = Math.ceil((acc.uRow + acc.lRow) / 2);
-        }
-        if (position === 'F') {
-          acc.uRow = Math.floor((acc.uRow + acc.lRow) / 2);
-        }
-        if (position === 'R') {
-          acc.lCol = Math.ceil((acc.lCol + acc.uCol) / 2);
-        }
-        if (position === 'L') {
-          acc.uCol = Math.floor((acc.lCol + acc.uCol) / 2);
-        }
-
-        return acc;
-      }, {
-        lRow: 0,
-        uRow: ROWS,
-        lCol: 0,
-        uCol: COLS
-      });
-    }).map(o => (o.lRow * 8) + o.lCol);
+    .map(position => {
+      return parseInt(
+        position.replace(/[BR]/g, '1').replace(/[FL]/g,'0'),
+        2
+      );
+    });
 };
 
 const partOne = () => {
@@ -43,9 +23,9 @@ const partTwo = () => {
   return getSeatIds()
     .sort((a, b) => a - b)
     .find((v, i, srcArr) => {
-      const adjIndex = i + srcArr[0];
+      const adjustedIndex = i + srcArr[0];
 
-      return v !== adjIndex;
+      return v !== adjustedIndex;
     }) - 1 // Find function gets the first seat ID after the empty one (which is effectively === emptySeatId + 1).;
 };
 
